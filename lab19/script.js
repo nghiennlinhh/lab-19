399c3706d17f18dda1a12d316d7f3c8712223c73
Nghiên Linh <dtmlinh89@gmail.com>
2024-06-12 22:47:13 +0900

    compeleted lab19

--- lab19/script.js
+++ lab19/script.js
@@ -8,9 +8,10 @@ document.querySelector('.score').textContent = 20 ;
 
 document.querySelector('.guess').value = 23 ;
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+
 
 document.querySelector('.check').addEventListener('click', function() {
  const guess = Number(document.querySelector('.guess').value);
@@ -23,9 +24,18 @@ document.querySelector('.check').addEventListener('click', function() {
 // When player wins
  }else if(guess === secretNumber) {
   document.querySelector('.message').textContent = '🎉 Correct Number!';
+  document.querySelector('.number').textContent = secretNumber;
+
 
   document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('input').style.backgroundColor = '#60b347';
+
   document.querySelector('.number').style.width = '140px';
+
+  if( score > highscore) {
+    highscore = score;
+    document.querySelector('.highscore').textContent = highscore;
+  }
 
   // When guess is too hight
  }else if(guess > secretNumber) {
@@ -49,4 +59,17 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('.score').textContent = 0;
   }
  }
+});
+document.querySelector('.again').addEventListener('click', function() {
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '130px';
+  
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('input').style.backgroundColor = '#222';
+  document.querySelector('.guess').value = '' ;
 });
